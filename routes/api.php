5137cb00968727ff62374c51aabc6eeb2bd8583f
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-01-26 10:39:58 +0100

    added register route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('register', 'Auth\RegisterController@register');
+
 Route::get('articles', 'ArticleController@index');
 Route::get('articles/{article}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
