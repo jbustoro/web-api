06eaf41da12ae579bfa2e7bceb4c2e44c0c10843
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2018-01-26 12:10:01 +0100

    added unauthenticated function to exeptions handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -57,5 +59,17 @@ class Handler extends ExceptionHandler
          }
 
          return parent::render($request, $exception);
+    }
+
+   /**
+    * Convert an authentication exception into an unauthenticated response.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Illuminate\Auth\AuthenticationException  $exception
+    * @return \Illuminate\Http\Response
+    */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthenticated'], 401);
     }
 }
